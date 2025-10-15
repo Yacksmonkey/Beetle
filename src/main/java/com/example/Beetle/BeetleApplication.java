89e70b46cbce37b035cad6d329af6aad9da4684a
EversonLandaeta <yacksmonkey@gmail.com>
2025-10-15 13:27:16 +0200

    BeetleApp

---
+++ src/main/java/com/example/Beetle/BeetleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Beetle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeetleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeetleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Beetle/BeetleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Beetle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeetleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

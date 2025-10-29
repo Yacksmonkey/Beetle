1ad429e215ef6d1ab4d0e787805ff5bd0a30a419
EversonLandaeta <yacksmonkey@gmail.com>
2025-10-29 15:20:07 +0100

    BeetleApp

--- src/main/java/com/example/Beetle/BeetleApplication.java
+++ src/main/java/com/example/Beetle/BeetleApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BeetleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BeetleApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BeetleApplication.class, args);
+    }
 }
--- src/main/java/com/example/Beetle/controller/PasswordResetController.java
+++ src/main/java/com/example/Beetle/controller/PasswordResetController.java
@@ -4,23 +4,25 @@ import com.example.Beetle.service.PasswordResetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/password")
-@CrossOrigin(origins = "*")
 public class PasswordResetController {
 
     @Autowired
     private PasswordResetService passwordResetService;
 
-
     @PostMapping("/request-reset")
-    public String requestReset(@RequestParam String email) {
+    public String requestReset(@RequestBody Map<String, String> request) {
+        String email = request.get("email");
         return passwordResetService.createPasswordResetToken(email);
     }
 
-
     @PostMapping("/reset")
-    public String resetPassword(@RequestParam String token, @RequestParam String newPassword) {
+    public String resetPassword(@RequestBody Map<String, String> request) {
+        String token = request.get("token");
+        String newPassword = request.get("newPassword");
         return passwordResetService.resetPassword(token, newPassword);
     }
 }

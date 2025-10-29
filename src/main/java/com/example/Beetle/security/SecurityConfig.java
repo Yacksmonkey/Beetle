8f6b12b4e763582b36cc7334f34a778c4bcd94fb
EversonLandaeta <yacksmonkey@gmail.com>
2025-10-29 15:26:32 +0100

    PasswordRecovery/cloudgmail

--- src/main/java/com/example/Beetle/security/SecurityConfig.java
+++ src/main/java/com/example/Beetle/security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        System.out.println("🚀 Security filter chain initialized correctly");
+        System.out.println("Security filter chain initialized correctly");
 
         http
                 .csrf(csrf -> csrf.disable())
